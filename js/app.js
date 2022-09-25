8a6a33f493675727e2008d2649f67b9dafcc8167
hoomanM77 <107865202+hoomanM77@users.noreply.github.com>
2022-09-25 17:37:16 +0330

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+window.addEventListener('load',()=>{
+    let browserAgent=window.navigator.userAgent
+    switch (true) {
+        case /edg/gi.test(browserAgent):{
+            let target=document.querySelector('.edge')
+            target.style.opacity='100%'
+        }
+        break
+        case /firefox/gi.test(browserAgent):{
+            let target=document.querySelector('.firefox')
+            target.style.opacity='100%'
+
+        }
+        break
+        case /opera/gi.test(browserAgent):{
+            let target=document.querySelector('.opera')
+            target.style.opacity='100%'
+
+        }
+        break
+        case /chrome/gi.test(browserAgent):{
+            let target=document.querySelector('.chrome')
+            target.style.opacity='100%'
+
+        }
+        break
+        case /safari/gi.test(browserAgent):{
+            let target=document.querySelector('.chrome')
+            target.style.opacity='100%'
+
+        }
+        break
+
+    }
+})
